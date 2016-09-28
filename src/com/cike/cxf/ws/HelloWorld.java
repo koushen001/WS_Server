611987cdc11ca3ffd5a6abf66471f75a3adb3b68
cike <875569847@qq.com>
2016-09-28 20:25:54 +0800

    WebService

--- src/com/cike/cxf/ws/HelloWorld.java
+++ src/com/cike/cxf/ws/HelloWorld.java
@@ -1,8 +1,14 @@
 package com.cike.cxf.ws;
 
+import java.util.List;
+
 import javax.jws.WebService;
+
+import com.cike.domain.Cat;
+import com.cike.domain.User;
 
 @WebService
 public interface HelloWorld {
 	String sayHi(String name);
+	List<Cat> getCatsByUser(User user);
 }
--- src/com/cike/cxf/ws/impl/HelloWorldWs.java
+++ src/com/cike/cxf/ws/impl/HelloWorldWs.java
@@ -1,10 +1,13 @@
 package com.cike.cxf.ws.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.jws.WebService;
 
 import com.cike.cxf.ws.HelloWorld;
+import com.cike.domain.Cat;
+import com.cike.domain.User;
 
 @WebService(endpointInterface="com.cike.cxf.ws.HelloWorld",serviceName="HelloWorldWs")
 public class HelloWorldWs implements HelloWorld {
@@ -12,6 +15,11 @@ public class HelloWorldWs implements HelloWorld {
 	@Override
 	public String sayHi(String name) {
 		return name + "，您好" + "，现在时间是：" + new Date();
+	}
+
+	@Override
+	public List<Cat> getCatsByUser(User user) {
+		return null;
 	}
 
 }
